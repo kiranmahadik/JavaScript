c68c45e7398c95552de3083b79986ea791b63290
Kiran Mahadik <58248911+kiranmahadik@users.noreply.github.com>
2022-05-26 14:22:23 +0530

    Add files via upload

--- 07-ChromeExtension/index.js
+++ 07-ChromeExtension/index.js
@@ -1,7 +1,11 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
 
 //localStorage.setItem("myName", "Kiran Mahadik");
 //let name1 = localStorage.getItem("myName");
@@ -11,41 +15,43 @@ const ulEl = document.getElementById("ul-el");
 
 //console.log(ulEl);
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
 
 // 1. Check if leadsFromLocalStorage is truthy
 // 2. If so, set myLeads to its value and call renderLeads()
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
 
 
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value);
-    console.log("Button clicked from addEventListener");
-    console.log(myLeads);
-    inputEl.value = "";
-
-    // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-    renderLeads();
-
-    // To verify that it works:
-    console.log(localStorage.getItem("myLeads"));
-});
+/*
+const tabs = [
+    { url: "https://www.linkedin.com/in/per-harald-borgen/" }
+]
+*/
 
 
-function renderLeads() {
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        //console.log(tabs[0].url);
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    })
+
+})
+
+
+
+function render(leads) {
     let listItems = "";  // this line is required for method-3
 
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
 
         //********************* method - 1 ***********************************************/
-        //ulEl.innerHTML = ulEl.innerHTML + "<li>" + myLeads[i] + "</li>";
+        //ulEl.innerHTML = ulEl.innerHTML + "<li>" + leads[i] + "</li>";
         //*****************************************************************************/
 
         //********************* method - 2 ***********************************************/
@@ -55,18 +61,18 @@ function renderLeads() {
         // 2] set text content
         // 3] append to ul
         /*const li = document.createElement("li");
-        li.textContent = myLeads[i];
+        li.textContent = leads[i];
         ulEl.append(li);*/
         //******************************************************************************/
 
         //******************** method - 3 ***********************************************/
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        // listItems += "<li><a target='_blank' href='" + leads[i] + "'>" + myLeads[i] + "</a></li>"
 
         // We can write above line in follwing way : 
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -75,4 +81,29 @@ function renderLeads() {
     ulEl.innerHTML = listItems;  // this line is required for method-3
 
 }
+
+
+deleteBtn.addEventListener("dblclick", function () {
+    //console.log("double clicked!");
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+    //console.log("Button clicked from addEventListener");
+    //console.log(myLeads);
+    inputEl.value = "";
+
+    // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+    render(myLeads);
+
+    // To verify that it works:
+    //console.log(localStorage.getItem("myLeads"));
+});
 
