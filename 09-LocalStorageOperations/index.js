8f8cf5587db0a31cf8dffd92ac362c6e08ca7ae2
Kiran Mahadik <58248911+kiranmahadik@users.noreply.github.com>
2022-05-26 14:24:33 +0530

    Add files via upload

---
+++ 09-LocalStorageOperations/index.js
@@ -0,0 +1,14 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+
+// Step-1 : Comment  last two lines and run the code
+
+//localStorage.setItem("myCredits", "100")
+
+// step-2 : comment above line and uncomment last two lines 
+//          and run the code.
+
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
